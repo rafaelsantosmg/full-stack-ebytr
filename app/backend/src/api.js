978bba131b928a4ac31cae441d068fc4a34575e6
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-26 13:50:06 -0300

    refactor(API)
    re3fatorada api para utilização de camada de rotas

--- app/backend/src/api.js
+++ app/backend/src/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const router = require('./routers');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(router.routerUser);
 
 module.exports = app;
