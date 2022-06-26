fff7342214e0ccc6b078079ad5f9b7f150fca4fd
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-26 17:18:24 -0300

    feat(Authentication)
    implementado a função para authenticação de usuário logado

---
+++ app/backend/src/authentication/authenticate.js
@@ -0,0 +1,19 @@
+const { decodeToken } = require('../utils/tokenJWT');
+const { User } = require('../database/models');
+
+module.exports = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+  const user = decodeToken(token);
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+  const findUser = await User.findOne({ where: { email: user.email } });
+  if (!findUser) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+  req.user = user;
+  next();
+};
--- app/backend/src/utils/tokenJWT.js
+++ app/backend/src/utils/tokenJWT.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+const JWT_SECRET = process.env.JWT_SECRET || 'MinhaSuperFraseSecreta';
+
 const generateToken = (user) =>
-  jwt.sign({ user }, process.env.JWT_SECRET, {
+  jwt.sign({ user }, JWT_SECRET, {
     expiresIn: '7d',
     algorithm: 'HS256',
   });
 
-const decodeToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
+const decodeToken = (token) => jwt.verify(token, JWT_SECRET);
 
 module.exports = {
   generateToken,
