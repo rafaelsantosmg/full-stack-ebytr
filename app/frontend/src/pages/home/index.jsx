16ef82a826ddc731eddb398760bd1bfaca2f4989
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-29 20:38:29 -0300

    feat(HomePage): implementado a estrutura da Home para exibição da tabela

--- app/frontend/src/pages/home/index.jsx
+++ app/frontend/src/pages/home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Table from './components/Table';
 import Forms from './components/Form';
 import './styles/styles.css';
@@ -7,16 +7,8 @@ import './styles/styles.css';
 export default function Home() {
   return (
     <Container>
-      <Row>
-        <Col>
-          <Forms />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Table />
-        </Col>
-      </Row>
+      <Forms />
+      <Table />
     </Container>
   );
 }
