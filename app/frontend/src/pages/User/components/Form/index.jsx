1e4dfb580c1e2df87481d7adef66d7fd883022da
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-29 20:39:15 -0300

    feat(UserCreate): implementado o endpoint para criação de usuário

--- app/frontend/src/pages/User/components/Form/index.jsx
+++ app/frontend/src/pages/User/components/Form/index.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
+import { Context } from '../../../../Context';
 
 export default function Forms() {
+  const { user, setUser } = useContext(Context);
+  const navigate = useNavigate();
+
+  const createUser = async (event) => {
+    event.preventDefault();
+    await axios.post('http://localhost:3333/users', user);
+    setUser({ displayName: '', email: '', password: '' });
+    navigate('/');
+  };
+
   return (
     <Form>
       <h1 className="title-user">Usuário</h1>
       <Form.Group className="mb-3" controlId="formBasicText">
         <Form.Label>Nome de Usuário</Form.Label>
-        <Form.Control type="text" placeholder="Enter userName" />
+        <Form.Control
+          type="text"
+          value={ user.displayName }
+          onChange={ (event) => setUser({ ...user, displayName: event.target.value }) }
+          placeholder="Enter userName"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          value={ user.email }
+          onChange={ (event) => setUser({ ...user, email: event.target.value }) }
+          placeholder="Enter email"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          value={ user.password }
+          onChange={ (event) => setUser({ ...user, password: event.target.value }) }
+          placeholder="Password"
+        />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button
+        variant="primary"
+        type="submit"
+        onClick={ createUser }
+      >
         Criar
       </Button>
     </Form>
