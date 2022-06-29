ab3bea77dba04d94c2166612361571dc8a9660a1
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-29 20:40:31 -0300

    feat(Cors)
    feita correção na chamda da função cors

--- app/backend/src/api.js
+++ app/backend/src/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const handleError = require('./middlewares/handleError');
 const router = require('./routers');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(router);
 
