993c7e1fa836ff28d08a1d1bfd28fa052e10d166
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-30 01:02:33 -0300

    refactor(Login Service)
    alterado o retorno do login acrescentando user e token

--- app/backend/src/services/loginServices.js
+++ app/backend/src/services/loginServices.js
@@ -4,13 +4,13 @@ const { compareHash } = require('../utils/bcryptGenerate');
 const { generateToken } = require('../utils/tokenJWT');
 
 const login = async (body) => {
-  const { displayName, email, password } = body;
+  const { email, password } = body;
   const user = await User.findOne({ where: { email } });
   if (!user) throw newError(404, 'User not found!');
   const validPassword = await compareHash(password, user.password);
   if (!validPassword) throw newError(401, 'Incorrect email or password');
-  const token = generateToken({ id: user.id, displayName, email });
-  return token;
+  const token = generateToken({ id: user.id, displayName: user.displayName, email });
+  return { user: { id: user.id, displayName: user.displayName, email }, token };
 };
 
 module.exports = {
