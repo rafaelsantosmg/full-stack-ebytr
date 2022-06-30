ed83ba0fda13fea2d19bc350fa1a5bde1102c443
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-30 00:38:54 -0300

    refactor(Login Controller)
    Alterado a forma como é feito o retorno do controller

--- app/backend/src/controllers/loginControllers.js
+++ app/backend/src/controllers/loginControllers.js
@@ -1,8 +1,8 @@
 const loginService = require('../services/loginServices');
 
 const login = async (req, res) => {
-  const token = await loginService.login(req.body);
-  return res.status(200).json({ token });
+  const response = await loginService.login(req.body);
+  return res.status(200).json(response);
 };
 
 module.exports = {
