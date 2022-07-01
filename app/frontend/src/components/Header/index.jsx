277791ec1f505fe4fa40488daf8bf56b9055374d
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-30 22:58:09 -0300

    feat(Header): Implementado o Header da aplicação

--- app/frontend/src/components/Header/index.jsx
+++ app/frontend/src/components/Header/index.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import { getUser } from '../../services/localStorage';
+import './styles/styles.css';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUser(getUser().user);
+  }, []);
+
+  const clearStorage = () => {
+    localStorage.removeItem('ebytr');
+    navigate('/');
+  };
+
   return (
-    <div>Header</div>
+    <Container className="header" fluid="md">
+      <Row>
+        <Col>
+          <h1>{ user.displayName }</h1>
+        </Col>
+        <Col>
+          <h1>Lista de Tarefas</h1>
+        </Col>
+        <Col style={ { textAlign: 'right' } }>
+          <i
+            aria-hidden
+            className="bi bi-door-closed"
+            style={ { fontSize: '40px', color: '#fff' } }
+            onClick={ clearStorage }
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
