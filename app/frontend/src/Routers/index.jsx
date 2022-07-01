fbc17a1383a8f0b2c460029e9118526f4b6325af
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-07-01 17:44:01 -0300

    feat(Router): Implementada a funcionalidade para entrar na routa login assim que inicia a pagina

--- app/frontend/src/Routers/index.jsx
+++ app/frontend/src/Routers/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from '../pages/home';
 import Login from '../pages/Login';
 import User from '../pages/User';
@@ -7,7 +7,8 @@ import User from '../pages/User';
 export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={ <Login /> } />
+      <Route path="/" element={ <Navigate to="/login" replace /> } />
+      <Route path="/login" element={ <Login /> } />
       <Route path="/home" element={ <Home /> } />
       <Route path="/user" element={ <User /> } />
     </Routes>
